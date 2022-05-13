179f3da7a82a0f9a5b79070f0eff71b55b8ad6b3
nildoeti <nildo@webstack.com.br>
2022-05-13 18:33:01 -0300

    adicionado primeiro exempl para pilhas

---
+++ estrutura-de-dados/stacks.js
@@ -0,0 +1,12 @@
+// exemplo 1
+const vetor = [];
+
+// empilhando os itens do vetor
+vetor.push(5); // primeiro a entrar
+vetor.push(2); // segundoo a entrar
+
+console.log(vetor); // [5, 2]
+
+// desenpilhando
+vetor.pop()/
+console.log(vetor); // 5
