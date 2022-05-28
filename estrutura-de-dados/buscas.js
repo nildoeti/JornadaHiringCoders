b4a7218cefdaa8bb5fb05bce67f00388a491975a
Nildo <nildo@webstack.com.br>
2022-05-28 17:58:24 -0400

    finalizado exemplo para busca binária

---
+++ estrutura-de-dados/buscas.js
@@ -0,0 +1,14 @@
+var valores = [5, 8, 10, 22, 45, 38];
+
+function busca(num) {
+    for (i = 0; i < 6; i++) {
+        if (num == valores[i]) { // retorna a posição dos valores no vetor
+            return i;
+        }
+    }
+    return -1;
+}
+
+// usando a busca
+console.log(busca(10));
+console.log(busca(50));
