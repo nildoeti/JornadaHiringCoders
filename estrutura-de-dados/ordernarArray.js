c4a676544aeb886310197457308d25650958f162
Nildo <nildo@webstack.com.br>
2022-05-30 10:31:00 -0300

    exemplo 2 adicioando ao projeto

--- estrutura-de-dados/ordernarArray.js
+++ estrutura-de-dados/ordernarArray.js
@@ -8,3 +8,12 @@ let arr = [9, -1, 5, -3, 60, 33, 90, 1, 34];
 arr.sort(funcaoOrdema);
 console.log(arr);
 
+
+// exemplo 2
+let myNumbers = [ 2, 0, 6, 3, -12, 8, -4, 90, 32, 900];
+
+let numberOrder = myNumbers.sort( function (x, y) {
+    return x > y;
+});
+
+console.log(numberOrder);
