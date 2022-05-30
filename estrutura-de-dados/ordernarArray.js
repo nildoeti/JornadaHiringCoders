69c20c2693c7d635d849a6202c08a1595c177fbc
Nildo <nildo@webstack.com.br>
2022-05-30 10:19:30 -0300

    adicionado exemplo 1 para ordenação de array

---
+++ estrutura-de-dados/ordernarArray.js
@@ -0,0 +1,10 @@
+// exemplo 1
+function funcaoOrdema(x, y) {
+    return x - y; // ordena valores em ordem crescente
+}
+
+let arr = [9, -1, 5, -3, 60, 33, 90, 1, 34];
+
+arr.sort(funcaoOrdema);
+console.log(arr);
+
